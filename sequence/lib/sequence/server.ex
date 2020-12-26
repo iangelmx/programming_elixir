06c2ff0a6244b4e7c434206fb10a5216d5c851db
Angel Ramírez <aramireza1506@alumno.ipn.mx>
2020-12-26 15:30:58 -0600

    Adds cast and format status to GenServer

--- sequence/lib/sequence/server.ex
+++ sequence/lib/sequence/server.ex
@@ -10,7 +10,15 @@ defmodule Sequence.Server do
   end
 
   def handle_call({:set_number, new_number}, _from, _current_number) do
- 	  { :reply, new_number, new_number }
+    { :reply, new_number, new_number }
+  end
+
+  def handle_cast({:increment_number, delta}, current_number) do
+    { :noreply, current_number + delta}
+  end
+
+  def format_status(_reason, [ _pdict, state ]) do
+    [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
  	end
 
 end
