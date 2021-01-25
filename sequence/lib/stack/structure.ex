93d1c798dcc513955ba3f2729c650ea2f0647e13
Angel Ramírez <aramireza1506@alumno.ipn.mx>
2021-01-24 23:11:31 -0600

    Refactors sequence server app

--- sequence/lib/stack/structure.ex
+++ sequence/lib/stack/structure.ex
@@ -1,30 +1,18 @@
-defmodule Stack.Structure do
-  @moduledoc """
-  This module is the implementation of a Right-Stack structure.
-  If starts as [1,2,3], the top will be "1".
+defmodule Sequence do
 
-  With call :pop it returns top of the stack ([head|_])
-  With call :get_stack it returns the current stack
+  @server Sequence.Server
 
-  """
-  use GenServer
-
-  @doc """
-  Recieves an list and builds the initial stack
-  """
-  @spec init( list() ) :: any()
-  def init(initial_stack) when is_list(initial_stack) do
-    { :ok, initial_stack }
+  def start_link(current_number) do
+    GenServer.start_link(@server, current_number, name: @server)
   end
-  def init(_), do: {:error, "The input given is not a list"}
 
-   def handle_call(:pop, _from, [top | tail_stack]) do
-     {:reply, top, tail_stack }
-   end
+  def next_number do
+    GenServer.call(@server, :next_number)
+  end
 
-   def handle_call(:get_stack, _from, current_stack) do
-     {:reply, current_stack, current_stack }
-   end
+  def increment_number(delta) do
+    GenServer.cast(@server, {:increment_number, delta})
+  end
 
 
 end
